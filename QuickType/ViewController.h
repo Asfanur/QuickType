f0065dec56bd8541379550885bc361e5d041c93c
Asfanur <asfanur.arafin@gmail.com>
2015-07-20 15:56:30 +1000

    Initial Commit

---
+++ QuickType/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickType
+//
+//  Created by Asfanur Arafin on 20/07/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuickType/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuickType
+//
+//  Created by Asfanur Arafin on 20/07/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
